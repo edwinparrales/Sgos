0db3cb350edf1c44ee823fc563b37d4777cbe76d
unknown <edwinparrales82@gmail.com>
2021-08-04 16:46:48 -0500

    first commit

---
+++ src/main/java/epv/sgos/demo/SgosApplication.java
@@ -0,0 +1,13 @@
+package epv.sgos.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/epv/sgos/demo/SgosApplicationTests.java
@@ -0,0 +1,13 @@
+package epv.sgos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
